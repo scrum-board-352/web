70155fed9d0a335084ad656f13846fd2b1a1e0f3
SUCHMOKUO <suchmokuo@gmail.com>
2020-03-17 18:37:16 +0800

    [SUCHMOKUO] add component Select

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import style from "./style.module.css";
+
+export interface Option {
+  name: string;
+  value: string;
+}
+
+type Props = {
+  options: Option[];
+  defaultValue: string;
+  onChange: (value: string) => void;
+};
+
+export default function Select(props: Props) {
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    props.onChange(e.target.value);
+  }
+
+  return (
+    <select
+      onChange={handleChange}
+      value={props.defaultValue}
+      className={style.select}
+    >
+      {props.options.map((o) => (
+        <option key={o.name} value={o.value}>
+          {o.name}
+        </option>
+      ))}
+    </select>
+  );
+}

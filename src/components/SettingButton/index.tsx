d12a7424db42393a5c067758a21a35f526baa39a
SUCHMOKUO <suchmokuo@gmail.com>
2020-03-18 15:29:53 +0800

    [SUCHMOKUO] add more icon type to SettingButton

--- src/components/SettingButton/index.tsx
+++ src/components/SettingButton/index.tsx
@@ -1,15 +1,31 @@
 import React, { useRef } from "react";
-import { IoMdSettings } from "react-icons/io";
+import { IoMdMore, IoMdSettings } from "react-icons/io";
+import { MdMoreHoriz } from "react-icons/md";
 import style from "./style.module.css";
 
 type Props = {
   size?: string;
   color?: string;
   hoverColor?: string;
+  type?: "dot-v" | "dot-h" | "gear";
   onClick?: () => void;
 };
 
+function icon(type: Props["type"]) {
+  switch (type) {
+    case "dot-v":
+      return <IoMdMore size="100%" />;
+    case "dot-h":
+      return <MdMoreHoriz size="100%" />;
+    case "gear":
+      return <IoMdSettings size="100%" />;
+    default:
+      return <></>;
+  }
+}
+
 export default function SettingButton(props: Props) {
+  const type = props.type ?? "gear";
   const btnRef = useRef<HTMLButtonElement>(null);
 
   return (
@@ -35,7 +51,7 @@ export default function SettingButton(props: Props) {
       }}
       onClick={props.onClick}
     >
-      <IoMdSettings size="100%" />
+      {icon(type)}
     </button>
   );
 }

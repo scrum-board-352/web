4d616a575314871ac8446df56da4e431807ffcb7
SUCHMOKUO <suchmokuo@gmail.com>
2020-03-18 17:33:16 +0800

    [SUCHMOKUO] add text to Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,8 +3,11 @@ import { Spinner } from "react-bootstrap";
 
 export default function Loading() {
   return (
-    <div className="w-100 h-100 d-flex align-items-center justify-content-center">
+    <div className="w-100 h-100 d-flex flex-column align-items-center justify-content-center">
       <Spinner animation="grow" variant="primary" />
+      <p className="mt-3 font-weight-bold" style={{ fontSize: "1.5rem" }}>
+        Loading...
+      </p>
     </div>
   );
 }

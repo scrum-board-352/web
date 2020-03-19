7ef956216a669bc06ec981cf0250b76b912534c2
SUCHMOKUO <suchmokuo@gmail.com>
2020-03-19 14:54:11 +0800

    [SUCHMOKUO] add scrollbar style to ScrollBox

--- src/components/ScrollBox/index.tsx
+++ src/components/ScrollBox/index.tsx
@@ -1,14 +1,37 @@
 import React from "react";
+import className from "utils/class-name";
 import style from "./style.module.css";
 
 type Props = {
+  type?: "vertical" | "horizontal";
+  className?: string;
   children: React.ReactNode;
 };
 
+function typeClass(type: Props["type"]) {
+  switch (type) {
+    case "horizontal":
+      return style.horizontal;
+    case "vertical":
+      return style.vertical;
+    default:
+      return "";
+  }
+}
+
 export default function ScrollBox(props: Props) {
+  const type = props.type ?? "horizontal";
   return (
     <div className={style.box}>
-      <div className={style.content_container}>{props.children}</div>
+      <div
+        className={className(
+          style.content_container,
+          typeClass(type),
+          props.className
+        )}
+      >
+        {props.children}
+      </div>
     </div>
   );
 }

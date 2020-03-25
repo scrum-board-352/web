30bf21a200878f38223625c28709ea968efe3523
SUCHMOKUO <suchmokuo@gmail.com>
2020-03-25 23:52:40 +0800

    [SUCHMOKUO] optimize useLoading

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 type AwaitReturnType<T> = T extends (...args: any[]) => Promise<infer V>
   ? V
@@ -8,18 +8,20 @@ type OpFunc = (...args: any[]) => any;
 
 export default function useLoading(): [boolean, typeof loadingOps] {
   const [loading, setLoading] = useState(false);
-
-  async function loadingOps<T extends OpFunc>(
-    f: T,
-    ...args: Parameters<T>
-  ): Promise<AwaitReturnType<T>> {
-    setLoading(true);
-    try {
-      return await f(...args);
-    } finally {
-      setLoading(false);
-    }
-  }
+  const loadingOps = useCallback(
+    async <T extends OpFunc>(
+      f: T,
+      ...args: Parameters<T>
+    ): Promise<AwaitReturnType<T>> => {
+      setLoading(true);
+      try {
+        return await f(...args);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setLoading]
+  );
 
   return [loading, loadingOps];
 }

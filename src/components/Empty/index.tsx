37acfe5429eeed3e0abb3568259524adf88ac012
SUCHMOKUO <suchmokuo@gmail.com>
2020-03-29 21:32:35 +0800

    [SUCHMOKUO] add size prop to Empty

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -3,14 +3,15 @@ import style from "./style.module.css";
 
 type Props = {
   message?: string;
+  size?: string;
 };
 
 const noDataPlaceholderSvg = process.env.PUBLIC_URL + "/img/no_data.svg";
 
 export default function Empty(props: Props) {
   return (
-    <div className={style.empty}>
-      <img src={noDataPlaceholderSvg} alt="" />
+    <div className={style.empty} style={{ fontSize: props.size }}>
+      <img src={noDataPlaceholderSvg} alt="" style={{ height: props.size ?? "10rem" }} />
       <p>{props.message ?? "There is nothing here..."}</p>
     </div>
   );

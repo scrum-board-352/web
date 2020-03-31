6111b238aae11fe98a87ceeb95f5a6bbac94753e
SUCHMOKUO <suchmokuo@gmail.com>
2020-04-01 01:09:05 +0800

    [SUCHMOKUO] add feature of inviting people to join team to User page

--- src/containers/User/index.tsx
+++ src/containers/User/index.tsx
@@ -1,12 +1,17 @@
 import Loading from "components/Loading";
+import { message } from "components/MessageBox";
+import ModalForm, { Template } from "components/ModalForm";
+import { selectTeamByUser, sendEmailToInviteReceiverJoinTeam } from "graphql/Team";
 import { selectUserBySubstring } from "graphql/User";
 import useLoading from "hooks/useLoading";
 import ProjectModel from "models/Project";
 import TeamModel from "models/Team";
 import UserModel from "models/User";
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useHistory, useParams } from "react-router-dom";
+import { useStore } from "rlax";
+import avatar from "utils/avatar";
 import Placeholder from "./Placeholder";
 import Projects from "./Projects";
 import projectSvg from "./projects.svg";
@@ -14,23 +19,23 @@ import style from "./style.module.css";
 import Teams from "./Teams";
 import teamSvg from "./teams.svg";
 
-const avatarPlaceholder = process.env.PUBLIC_URL + "/img/user.svg";
-
-function avatarUrl(url?: string) {
-  return url ? url : avatarPlaceholder;
-}
+type FormValues = {
+  teamId: string;
+};
 
 export default function User() {
   const { username } = useParams();
   const history = useHistory();
-  const [userInfo, setUserInfo] = useState<UserModel.PublicInfo>({
+  const [userInfo, setUserInfo] = useState<UserModel.PrivateInfo>({
     id: "",
     name: "",
     avatar: "",
+    email: "",
   });
   const [projects, setProjects] = useState<ProjectModel.Info[]>([]);
   const [teams, setTeams] = useState<TeamModel.Info[]>([]);
   const [loading, loadingOps] = useLoading();
+  const currentUser: UserModel.PrivateInfo = useStore("user");
 
   useEffect(() => {
     loadingOps(async () => {
@@ -51,18 +56,90 @@ export default function User() {
     });
   }, []);
 
+  const [showSelectTeamForm, setShowSelectTeamForm] = useState(false);
+  const [inviteLoading, inviteLoadingOps] = useLoading();
+
+  async function handleInviteSubmit(values: FormValues) {
+    if (!values.teamId) {
+      message({
+        title: "Invalid Input!",
+        type: "error",
+        content: "You should select a team for user to join in!",
+      });
+      setShowSelectTeamForm(false);
+      return;
+    }
+    const res = await inviteLoadingOps(sendEmailToInviteReceiverJoinTeam, {
+      receiver: userInfo.name,
+      receiverMail: userInfo.email,
+      teamId: values.teamId,
+    });
+    if (res.success) {
+      message({
+        title: "Email Sent Succeed!",
+        type: "success",
+        content: res.message,
+      });
+    } else {
+      message({
+        title: "Email Sent Failed!",
+        type: "error",
+        content: res.message,
+      });
+    }
+    setShowSelectTeamForm(false);
+  }
+
+  const [myTeams, setMyTeams] = useState<Array<TeamModel.Info>>([]);
+
+  useEffect(() => {
+    (async () => {
+      const myTeams = await selectTeamByUser({ username: currentUser.name });
+      setMyTeams(myTeams);
+    })();
+  }, []);
+
+  const formTemplates: Array<Template<FormValues>> = [
+    {
+      label: "Which team?",
+      name: "teamId",
+      type: "select",
+      options: myTeams.map((team) => ({
+        label: team.name,
+        value: team.id,
+      })),
+    },
+  ];
+
   return (
     <div className="vh-100 vw-100">
       {loading ? (
         <Loading />
       ) : (
         <>
+          <ModalForm<FormValues>
+            title="Invite User"
+            templates={formTemplates}
+            loading={inviteLoading}
+            show={showSelectTeamForm}
+            onClose={() => setShowSelectTeamForm(false)}
+            onSubmit={handleInviteSubmit}
+          />
           <div className={style.top_banner}></div>
           <Container>
             <Row>
               <Col md={3} className={style.user_info_side}>
-                <img src={avatarUrl(userInfo.avatar)} alt="" className={style.avatar} />
+                <img src={avatar(userInfo.avatar)} alt="" className={style.avatar} />
                 <span className={style.username}>{userInfo.name}</span>
+                {userInfo.id !== currentUser.id && (
+                  <Button
+                    variant="link"
+                    size="sm"
+                    className="mt-3"
+                    onClick={() => setShowSelectTeamForm(true)}>
+                    邀请用户加入团队
+                  </Button>
+                )}
               </Col>
               <Col md={9}>
                 <div className={style.content_container}>

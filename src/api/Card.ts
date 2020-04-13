5d1b9cbb4520f87abb020d88f356363e3e3fc4f3
SUCHMOKUO <suchmokuo@gmail.com>
2020-04-13 22:42:57 +0800

    [SUCHMOKUO] change import name in api/Card

--- src/api/Card.ts
+++ src/api/Card.ts
@@ -1,18 +1,18 @@
 import CardMutation from "api/mutation/CardMutation";
 import CardQuery from "api/query/CardQuery";
-import { default as Card } from "models/Card";
+import CardModel from "models/Card";
 import ResultOutput from "models/ResultOutput";
 import { setApiMappingName } from "./base/api-name-mapping";
 import client from "./base/client";
 
 setApiMappingName(createCard, "createCard");
-export async function createCard(createInfo: Card.CreateInfo): Promise<Card.Info> {
+export async function createCard(createInfo: CardModel.CreateInfo): Promise<CardModel.Info> {
   const data = await client.request(CardMutation.createCard, createInfo);
   return data.createCard;
 }
 
 setApiMappingName(updateCard, "updateCard");
-export async function updateCard(updateInfo: Card.UpdateInfo): Promise<Card.Info> {
+export async function updateCard(updateInfo: CardModel.UpdateInfo): Promise<CardModel.Info> {
   const data = await client.request(CardMutation.updateCard, updateInfo);
   return data.updateCard;
 }
@@ -20,7 +20,7 @@ export async function updateCard(updateInfo: Card.UpdateInfo): Promise<Card.Info
 setApiMappingName(selectCardsByBoardId, "selectCardsByBoardId");
 export async function selectCardsByBoardId(boardId: {
   boardId: string;
-}): Promise<Array<Card.Info>> {
+}): Promise<Array<CardModel.Info>> {
   const data = await client.request(CardQuery.selectCardsByBoardId, boardId);
   return data.selectCardsByBoardId;
 }

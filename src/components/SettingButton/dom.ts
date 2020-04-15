7a0435e798bb893a9d9b8aa965375677ddc1b56e
SUCHMOKUO <suchmokuo@gmail.com>
2020-04-15 18:38:25 +0800

    [SUCHMOKUO] optimized SettingButton's performance

---
+++ src/components/SettingButton/dom.ts
@@ -0,0 +1,116 @@
+import style from "./style.module.css";
+
+export interface MenuItem {
+  label: string;
+  onClick?: () => void;
+}
+
+function initMenu() {
+  const menu = document.createElement("div");
+  menu.classList.add(style.menu);
+  document.body.appendChild(menu);
+  return menu;
+}
+
+const menu = initMenu();
+let lastClickedBtn: HTMLButtonElement | null = null;
+const menuItemElemPool: Array<HTMLButtonElement> = [];
+
+function putMenuItemElemToPool(btn: HTMLButtonElement) {
+  menuItemElemPool.push(btn);
+}
+
+function getMenuItemElem(item: MenuItem) {
+  let btn = menuItemElemPool.shift();
+  if (!btn) {
+    btn = document.createElement("button");
+    btn.classList.add(style.item);
+  }
+  btn.textContent = item.label;
+  btn.onclick = item.onClick ?? null;
+  return btn;
+}
+
+function isMenuShowing() {
+  return menu.classList.contains(style.show);
+}
+
+function clearMenu() {
+  const buttons = menu.children;
+  const len = buttons.length;
+  for (let i = 0; i < len; i++) {
+    putMenuItemElemToPool(buttons[i] as HTMLButtonElement);
+  }
+
+  while (menu.firstChild) {
+    menu.removeChild(menu.lastChild as Node);
+  }
+}
+
+function setMenuItems(items?: Array<MenuItem>) {
+  clearMenu();
+  if (!items) {
+    return;
+  }
+  const f = document.createDocumentFragment();
+  for (const item of items) {
+    const itemElem = getMenuItemElem(item);
+    f.appendChild(itemElem);
+  }
+  menu.appendChild(f);
+}
+
+function updateMenuAndShow(pageX: number, pageY: number, menuItems?: Array<MenuItem>) {
+  setMenuItems(menuItems);
+  const rect = menu.getBoundingClientRect();
+  const menuWidth = rect.width;
+  const menuHeight = rect.height;
+  const availableRight = window.innerWidth - pageX;
+  const availableBottom = window.innerHeight - pageY;
+  menu.style.left = `${availableRight < menuWidth ? pageX - menuWidth : pageX}px`;
+  menu.style.top = `${availableBottom < menuHeight ? pageY - menuHeight : pageY}px`;
+  menu.classList.add(style.show);
+}
+
+function hideMenu(cb: any) {
+  menu.classList.remove(style.show);
+  menu.addEventListener("transitionend", cb, { once: true });
+  removeHideMenuEventHandler();
+}
+
+const hideMenuEvents = ["click", "wheel"];
+
+function removeHideMenuEventHandler() {
+  hideMenuEvents.forEach((eventName) => {
+    document.removeEventListener(eventName, hideMenu);
+  });
+}
+
+function setHideMenuEventHandler() {
+  hideMenuEvents.forEach((eventName) => {
+    document.addEventListener(eventName, hideMenu, { once: true });
+  });
+}
+
+export function showMenuClickHandler(
+  e: React.MouseEvent<HTMLButtonElement>,
+  menuItems?: Array<MenuItem>
+) {
+  e.stopPropagation();
+
+  const isShowing = isMenuShowing();
+  if (isShowing && e.target === lastClickedBtn) {
+    return;
+  }
+  lastClickedBtn = e.target as HTMLButtonElement;
+
+  const { pageX, pageY } = e;
+
+  if (isShowing) {
+    hideMenu(() => updateMenuAndShow(pageX, pageY, menuItems));
+  } else {
+    updateMenuAndShow(pageX, pageY, menuItems);
+  }
+
+  setHideMenuEventHandler();
+}
--- src/components/SettingButton/index.tsx
+++ src/components/SettingButton/index.tsx
@@ -1,85 +1,10 @@
 import React, { useRef } from "react";
 import { IoMdMore, IoMdSettings } from "react-icons/io";
 import { MdKeyboardArrowDown, MdMoreHoriz } from "react-icons/md";
+import { MenuItem as _MenuItems, showMenuClickHandler } from "./dom";
 import style from "./style.module.css";
 
-export interface MenuItem {
-  label: string;
-  onClick?: () => void;
-}
-
-function initMenu() {
-  const menu = document.createElement("div");
-  menu.classList.add(style.menu);
-  document.body.appendChild(menu);
-  return menu;
-}
-
-const menu = initMenu();
-
-let curBtn: HTMLButtonElement | null = null;
-
-function isMenuShowing() {
-  return menu.classList.contains(style.show);
-}
-
-function clearMenu() {
-  while (menu.firstChild) {
-    menu.removeChild(menu.lastChild as Node);
-  }
-}
-
-function setMenuItems(items?: Array<MenuItem>) {
-  clearMenu();
-  if (!items) {
-    return;
-  }
-  const f = document.createDocumentFragment();
-  for (const item of items) {
-    const itemElem = document.createElement("button");
-    itemElem.appendChild(document.createTextNode(item.label));
-    itemElem.classList.add(style.item);
-    if (item.onClick) {
-      itemElem.addEventListener("click", item.onClick);
-    }
-    f.appendChild(itemElem);
-  }
-  menu.appendChild(f);
-}
-
-function updateMenuAndShow(pageX: number, pageY: number, menuItems?: Array<MenuItem>) {
-  setMenuItems(menuItems);
-  const rect = menu.getBoundingClientRect();
-  const menuWidth = rect.width;
-  const menuHeight = rect.height;
-  const availableRight = window.innerWidth - pageX;
-  const availableBottom = window.innerHeight - pageY;
-  menu.style.left = `${availableRight < menuWidth ? pageX - menuWidth : pageX}px`;
-  menu.style.top = `${availableBottom < menuHeight ? pageY - menuHeight : pageY}px`;
-  menu.classList.add(style.show);
-}
-
-function hideMenu(cb: any) {
-  menu.classList.remove(style.show);
-  if (typeof cb === "function") {
-    menu.addEventListener("transitionend", cb, { once: true });
-  }
-  removeHideMenuHandler();
-}
-
-const hideMenuEvents = ["click", "wheel"];
-
-function removeHideMenuHandler() {
-  hideMenuEvents.forEach((eventName) => {
-    document.removeEventListener(eventName, hideMenu);
-  });
-}
-
-function setHideMenuEventHandler() {
-  hideMenuEvents.forEach((eventName) => {
-    document.addEventListener(eventName, hideMenu, { once: true });
-  });
-}
+export type MenuItem = _MenuItems;
 
 type Props = {
   size?: string;
@@ -108,26 +33,6 @@ export default function SettingButton(props: Props) {
   const type = props.type ?? "gear";
   const btnRef = useRef<HTMLButtonElement>(null);
 
-  function showMenu(e: React.MouseEvent) {
-    e.stopPropagation();
-
-    const isShowing = isMenuShowing();
-    if (isShowing && btnRef.current === curBtn) {
-      return;
-    }
-    curBtn = btnRef.current;
-
-    const { pageX, pageY } = e;
-
-    if (isShowing) {
-      hideMenu(() => updateMenuAndShow(pageX, pageY, props.menuItems));
-    } else {
-      updateMenuAndShow(pageX, pageY, props.menuItems);
-    }
-
-    setHideMenuEventHandler();
-  }
-
   return (
     <button
       ref={btnRef}
@@ -149,7 +54,7 @@ export default function SettingButton(props: Props) {
         width: props.size ?? "",
         height: props.size ?? "",
       }}
-      onClick={showMenu}>
+      onClick={(e) => showMenuClickHandler(e, props.menuItems)}>
       {icon(type)}
     </button>
   );

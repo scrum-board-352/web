7ce76a1719d6c2418b9f2b3a112fdbfa0b48a4b0
SUCHMOKUO <suchmokuo@gmail.com>
2020-07-03 02:58:46 +0800

    [SUCHMOKUO] fix deploy

--- server/app.js
+++ server/app.js
@@ -1,24 +1,23 @@
 const Koa = require("koa");
-const static = require("koa-static-cache");
+const staticFiles = require("koa-static-cache");
 const path = require("path");
 const fs = require("fs");
 
 const PORT = Number(process.env.PORT) || 3000;
-const STATIC = process.env.STATIC || "../build";
+const STATIC_PATH = path.join(__dirname, "build");
 
 const app = new Koa();
 
-const staticPath = path.resolve(STATIC);
 app.use(
-  static({
-    dir: staticPath,
+  staticFiles({
+    dir: STATIC_PATH,
     buffer: true,
     gzip: true,
     usePrecompiledGzip: true,
   })
 );
 
-const indexHtml = fs.readFileSync(path.join(staticPath, "index.html"));
+const indexHtml = fs.readFileSync(path.join(STATIC_PATH, "index.html"));
 app.use((ctx) => {
   ctx.type = "text/html; charset=utf-8";
   ctx.body = indexHtml;
